e9d7b8b19a607cb0c202498d1f8a35433bf8c91b
Simon Dahlström <notdonshipley@gmail.com>
2019-10-02 14:39:46 +0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace Abstrakta_klasser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Cat jaffe = new Cat(true, "Norsk skogskatt", "3");
+            jaffe.Talk(10);            
+        }
+    }
+
+
+
+
+
+
+    public abstract class Animal
+    {    
+        public abstract void Talk(int times);
+    }
+
+
+
+
+
+    public class Feline : Animal
+    {
+        public override void Talk(int times){
+
+            int i = 0;
+
+            do{
+                System.Console.WriteLine("Meow");
+
+                i++;
+            }while(i < times);
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public class Cat : Feline
+    {
+        public bool isAlive { get; set; }
+        public string breed { get; set; }
+        public string weight { get; set; }
+
+        public Cat(bool isAlive, string breed, string weight)
+        {
+            this.isAlive = isAlive;
+            this.breed = breed;
+            this.weight = weight;
+        }       
+    }
+}
